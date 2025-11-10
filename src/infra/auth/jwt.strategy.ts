3dabb5f31a22236d5f065545c90331363a6603da
joas-vieira <joas.vieira.dev@gmail.com>
2025-11-10 16:59:08 -0300

    feat: add infra layer

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Env } from '@/env';
+import { Env } from '@/infra/env';
 import z from 'zod';
 
 export const tokenPayloadSchema = z.object({
